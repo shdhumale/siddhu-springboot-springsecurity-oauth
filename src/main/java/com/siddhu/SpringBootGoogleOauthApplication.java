aea3cfa78adc4aebeb50c8600f6014efbd4b09c4
shdhumale <shdhumale@gmail.com>
2021-04-29 17:41:19 +0530

    First Commit

---
+++ src/main/java/com/siddhu/SpringBootGoogleOauthApplication.java
@@ -0,0 +1,13 @@
+package com.siddhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoogleOauthApplication.class, args);
+	}
+
+}
